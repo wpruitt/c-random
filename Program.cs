422298e5eddbf1c4c5fc68e94428f1dd4ca4bbc5
Willie Pruitt <w_pruitt@hotmail.com>
2017-07-12 21:42:59 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> randomNumbers = new List<int>();
+            while (randomNumbers.Count < 20)
+            {
+            randomNumbers.Add(random.Next(1, 51));
+            }
+            foreach (int number in randomNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            List<int> squaredNumbers = new List<int>();
+            foreach (int number in randomNumbers)
+            {
+                squaredNumbers.Add(number*number);
+            }
+            foreach (int number in squaredNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine($"squaredNumbers contains {squaredNumbers.Count} numbers.");
+            squaredNumbers.RemoveAll(number => number %2 != 0);
+            foreach (int number in squaredNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine($"squaredNumbers now contains {squaredNumbers.Count} numbers.");
+
+            
+        }
+    }
+
+}
